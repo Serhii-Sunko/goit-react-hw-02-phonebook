e2d3b74934abade101030335db18a84714e087c1
Serhii-Sunko <sergeysunko01@gmail.com>
2022-02-18 09:46:48 +0200

    initial fixing files

---
+++ src/Components/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import ContactFilter from './ContactFilter/ContactFilter';
+import { v4 as uuid } from 'uuid';
+import Container from './Container/Container';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = contact => {
+    const id = uuid();
+    const { name } = contact;
+
+    if (this.state.contacts.filter(contact => contact.name === name).length > 0) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { id, ...contact }],
+    }));
+  };
+
+  deleteItem = id => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts.filter(item => item.id !== id)],
+    }));
+  };
+
+  onFilterChange = e => {
+    const filter = e.target.value;
+    this.setState({ filter: filter.toLowerCase() });
+  };
+
+  getFilteredContacts = () => {
+    const { filter, contacts } = this.state;
+    return contacts.filter(({ name }) => name.toLowerCase().includes(filter));
+  };
+
+  render() {
+    return (
+      <>
+        <Container title="Phonebook">
+          <ContactForm onSubmit={this.addContact} />
+        </Container>
+        <Container title="Contacts">
+          <ContactFilter onChange={this.onFilterChange} />
+
+          <ContactList onDelete={this.deleteItem} items={this.getFilteredContacts()} />
+        </Container>
+      </>
+    );
+  }
+}
+
+export default App;
